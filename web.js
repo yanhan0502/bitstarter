fbf99e7c6bdd359eee4d0ec00bfaea58e57b4650
yanhan0502 <yanhan0502@gmail.com>
2013-07-09 16:02:28 +0000

    web.js

--- web.js
+++ web.js
@@ -3,15 +3,18 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var buf = new Buffer(20);
+
 fs.readFile("index.html", function(err, data){
   if (err) throw err;
-  buf.write(data,"utf-8");
+  var buf = new Buffer(data, "utf-8");
+  app.get('/', function(request, response) {
+    response.send(buf.toString("utf-8"));
+  });
 });
 
-app.get('/', function(request, response) {
-  response.send(buf.toString("utf-8"));
-});
+//app.get('/', function(request, response) {
+//  response.send(buf.toString("utf-8"));
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
